fbf85f0bd3b61d2b211a3095b18690c46967fd93
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-10-03 03:09:19 +0900

    화면 제어 이벤트 구현 및 추가

--- src/atoms/Input.jsx
+++ src/atoms/Input.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import styled from 'styled-components';
 
 
 const Inputs = styled.input`
-
     border-radius : 3px;
     width : ${props => props.styled.width};
     margin : ${props => props.styled.margin};
     padding : ${props => props.styled.padding};  
 `
 
-export const Input = ({ styled, onChange, values, name, placeholder }) => { //외부에 동작을 위임하는 Input 컴포넌트 작성
 
-    return(
-        <Inputs name={name} value={values} onChange={onChange} styled={styled} placeholder={placeholder} />
-    )
-}
+export const Input = forwardRef(({name, values, onChange, placeholder, styled}, ref) => {
+
+        return  <Inputs name={name} value={values} onChange={onChange} styled={styled} placeholder={placeholder} ref={ref} />
+
+    });
+    
+
+ 
--- src/molecule/SearchInput.jsx
+++ src/molecule/SearchInput.jsx
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button } from '../atoms/Button';
 import { Input } from '../atoms/Input';
 import { FaSearch } from 'react-icons/fa';
 
-export const SearchInput = ({ styled, name, values, onChange, onSubmit }) => {
-
+export const SearchInput = ({ styled, name, values, onChange }) => {
+    
+    const searchref = useRef();
     const [toggle, setToggle] = useState(true); //toggle을 내부에 담을 것인가? 외부에 위임할 것인가 자체 기능이므로 컴포넌트 내부에 내장
     //SearchInput 분자 고유 기능이므로 state로 toggle 내장
-  
+    
+    const onBtnSearch = () => {
+        setToggle(prev => !prev);
+    };
+
+    useEffect(() => {
+        function handleClickOutside(e) {
+            console.log(searchref.current);
+          if (
+            searchref.current &&
+            !searchref.current.contains(e.target)
+          ) {
+            setToggle(true); 
+          }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+      }, [searchref]);
+
     const btnstyles = {...styled.btn };
     const inputstyles = {  ...styled.input, width : '35%'};
     
@@ -15,9 +36,14 @@ export const SearchInput = ({ styled, name, values, onChange, onSubmit }) => {
     return(
         <React.Fragment>
             { toggle ? //버튼이 눌리면 Input바가 나타나게함
-                <Button onClicks={() => setToggle(prev => !prev)} styled={btnstyles}><FaSearch /></Button> 
+                <Button onClicks={onBtnSearch} styled={btnstyles}><FaSearch /></Button> 
                 : 
-                <Input name={name} values={values} onChange={onChange} styled={inputstyles} placeholder='원하는 작품을 검색해주세요...' />
+                <Input name={name}
+                     values={values} 
+                        onChange={onChange} 
+                            styled={inputstyles}
+                             placeholder='원하는 작품을 검색해주세요...'
+                              ref={searchref}/>
             }
         </React.Fragment>
     )

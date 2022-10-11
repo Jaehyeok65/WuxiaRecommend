4c5f9cc96c093feb71e5d789dd9fc00f106d92de
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-10-11 23:35:10 +0900

    Product 좋아요(like) 추가

--- src/molecule/Product/index.jsx
+++ src/molecule/Product/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Title from '../../atoms/Title';
 import { Text } from '../../atoms/Text';
+import { FaHeart } from "react-icons/fa";
+import Icon from '../../atoms/Icon';
 
 
-const Product = ( { product, styled }) => {
+const Product = ( { product, styled, icon, setIcon }) => {
 
 
     return(
@@ -11,8 +13,11 @@ const Product = ( { product, styled }) => {
         <img src={product.url} alt={product.title} />
         <div>
             <Title styled={styled.title}>{product.title}</Title>
+            <Text styled={styled.text}>{product.subtitle}</Text>
             <Text styled={{...styled.text, fontSize : '13px'}}>{product.content}</Text>
-            <Text styled={styled.text}>조회수 : {product.view}</Text>
+            <Icon styled={{fontSize : '15px', color : 'gray'}} icon={icon} setIcon={setIcon}><FaHeart /></Icon>
+            <span style={{fontSize : '14px', verticalAlign : 'top', marginLeft : '8px'}}>{product.like}</span>
+            <Text styled={{...styled.text, marginTop : '5%'}}>조회수 : {product.view}</Text>
             <a href='/'>바로가기 링크</a>
         </div>
         </>

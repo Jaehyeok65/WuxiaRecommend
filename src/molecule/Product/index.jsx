a111c6f0766e2d3678a6840c453dd7e0749a2f05
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-10-11 17:31:47 +0900

    Product 컴포넌트 구현

--- src/molecule/Product/index.jsx
+++ src/molecule/Product/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Title from '../../atoms/Title';
+import { Text } from '../../atoms/Text';
 
 
-const Product = ( { product }) => {
+const Product = ( { product, styled }) => {
 
 
     return(
         <>
         <img src={product.url} alt={product.title} />
         <div>
-            <h2>{product.title}</h2>
+            <Title styled={styled.title}>{product.title}</Title>
+            <Text styled={{...styled.text, fontSize : '13px'}}>{product.content}</Text>
+            <Text styled={styled.text}>조회수 : {product.view}</Text>
             <a href='/'>바로가기 링크</a>
         </div>
         </>

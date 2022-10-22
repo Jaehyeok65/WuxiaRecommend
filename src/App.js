aecb4d4c1e084b58ff89dde922e5e241b26ac60d
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-10-22 14:22:30 +0900

    쿠키를 통해 로그인 상태 유지 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './organism/Sidebar';
+import WuxiaHead from './organism/WuxiaHead';
+import Modal from './molecule/Modal';
+import Login from './organism/Login';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+import List from './pages/List';
+
 
 function App() {
+
+
+  
+  const [sidetoggle, setSideToggle] = useState(false); //사이드바 관련 토글
+  const [logintoggle, setLoginToggle] = useState(false); //로그인 모달 관련 토글
+  const [isLogin, setIsLogin] = useState(true); //로그인창인지 회원가입창인지 구분
+  const [loginstate, setLoginstate] = useState(() => document.cookie.split("=")[1] || false); //true라면 login이 필요한 상태
+  
+  console.log(loginstate);
+ 
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <WuxiaHead onClicks={() => setSideToggle(prev => !prev)} onClickss={() => setLoginToggle(prev => !prev)}
+        loginstate={!loginstate} setLoginstate={() => setLoginstate(prev => !prev)} />
+      <Sidebar onClicks={() => setSideToggle(prev => !prev)} toggle={sidetoggle} />
+      <Modal toggle={logintoggle} onClicks={() => setLoginToggle(prev => !prev)}>
+        <Login onClose={() => setLoginToggle(prev => !prev)} isLogin={isLogin} setIsLogin = {() => setIsLogin(prev => !prev)} setLoginstate={() => setLoginstate(prev => !prev)} />
+      </Modal>
+      <Routes>
+        <Route exact path='/' element={<Main />} />
+        <Route exact path='/detail/:title' element={<Detail />} />
+        <Route exact path='/menu/:title' element={<List />} />
+      </Routes>
+    </Router>
   );
 }
 

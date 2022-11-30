fe978f2b077c53383567942ff734812ef6f7d76d
Lee, Jaehyeok <dlwogur15@gmail.com>
2022-11-30 21:15:02 +0900

    Presentational Comment Write 컴포넌트 작성

--- src/pages/CommentWrite/index.jsx
+++ src/pages/CommentWrite/index.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Input } from '../../atoms/Input';
 import MainFrame from '../MainFrame';
 import styled from 'styled-components';
-import { Formatting } from '../../api/CommentAPI';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { getCommentSubmit } from '../../redux/action';
 
 const WriteArea = styled.div`
     display : flex;
@@ -19,58 +15,7 @@ const TextArea = styled.textarea`
     margin-top : 1%;
 `
 
-const CommentWrite = ( { loginstate, nickname }) => {
-
-
-    const [comment, setComment] = useState({
-        wuxia : '',
-        title : '',
-        content : '',
-        writer : nickname,
-        date : Formatting(new Date()),
-        view : 0,
-        recommend : 0
-    });
-
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    useEffect( () => {
-        if(!loginstate) {
-            window.alert("로그인이 필요합니다.");
-            navigate('/community');
-        }
-        
-    }, [])
-
-    useEffect(() => {
-        setComment({...comment, writer : nickname});
-    },[nickname]);
-
-
-
-    
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if(comment.title.trim() === '') {
-            window.alert("제목을 입력하세요");
-            return;
-        }
-        if(comment.content.trim() === '') {
-            window.alert("내용을 입력하세요");
-            return;
-        }
-        dispatch(getCommentSubmit(comment,"최신순"));
-        navigate(`/community`);
-    };
-
-    const onChange = (e) => {
-        const { name, value } = e.target;
-        setComment({...comment,
-            [name] : value
-        });
-    }
+const CommentWrite = ( { comment, onSubmit, onChange }) => {
 
 
     return(

bad9d3fc32be6d15acf6da5f1cfcf788c1e5c703
Lee, Jaehyeok <dlwogur15@gmail.com>
2023-03-10 23:25:39 +0900

    feat : 회원가입 버튼 및 모달 창 생성

--- src/pages/Navigate/index.jsx
+++ src/pages/Navigate/index.jsx
@@ -1,38 +1,55 @@
 import React, { useState } from 'react';
-import WuxiaHead from '../../organism/WuxiaHead';
+import Header from '../../organism/Header';
 import Sidebar from '../../organism/Sidebar';
 import Modal from '../../molecule/Modal';
 import Login from '../../organism/Login';
 import { useSelector, useDispatch } from 'react-redux';
 import { LoginModal } from '../../redux/reducers/modal';
+import { SignUpModal } from '../../redux/reducers/modal';
+import SignUp from '../../organism/SignUp';
 
-
-const Navigate = ( { loginstate, setLoginstate, setNickname } ) => {
-
-
+const Navigate = ({ loginstate, setLoginstate, setNickname }) => {
     const [sidetoggle, setSideToggle] = useState(false); //사이드바 관련 토글
-    const logintoggle = useSelector(state => state.modal.login.data);
+
+    const logintoggle = useSelector((state) => state.modal.login.data);
+
+    const signuptoggle = useSelector((state) => state.modal.signup.data);
 
     const dispatch = useDispatch();
 
-    const ShowModal = (data) => dispatch(LoginModal(data));
+    const ShowLoginModal = (data) => dispatch(LoginModal(data));
 
-    return(
+    const ShowSignUpModal = (data) => dispatch(SignUpModal(data));
+
+    return (
         <React.Fragment>
-            <WuxiaHead onClicks={() => setSideToggle(prev => !prev)} onClickss={() => ShowModal(true)}
-            loginstate={!loginstate} setLoginstate={() => setLoginstate(prev => !prev)} setNickname={() => setNickname(null)}/>
-            <Sidebar onClicks={() => setSideToggle(prev => !prev)} toggle={sidetoggle} />
+            <Header
+                onClicks={() => setSideToggle((prev) => !prev)}
+                onClickLoginModal={() => ShowLoginModal(true)}
+                loginstate={!loginstate}
+                setLoginstate={() => setLoginstate((prev) => !prev)}
+                setNickname={() => setNickname(null)}
+                onClickSignUpModal={() => ShowSignUpModal(true)}
+            />
+            <Sidebar
+                onClicks={() => setSideToggle((prev) => !prev)}
+                toggle={sidetoggle}
+            />
             <Modal toggle={logintoggle}>
-            <Login setLoginstate={() => setLoginstate(prev => !prev)} setNickname={setNickname} onClose={() => ShowModal(false)}/>
+                <Login
+                    setLoginstate={() => setLoginstate((prev) => !prev)}
+                    setNickname={setNickname}
+                    onClose={() => ShowLoginModal(false)}
+                />
+            </Modal>
+            <Modal toggle={signuptoggle}>
+                <SignUp
+                    setNickname={setNickname}
+                    onClose={() => ShowSignUpModal(false)}
+                />
             </Modal>
         </React.Fragment>
-    )
-
+    );
 };
 
-
 export default React.memo(Navigate);
-
-
-
-
--- src/redux/reducers/modal.jsx
+++ src/redux/reducers/modal.jsx
@@ -2,13 +2,20 @@
 
 export const LOGIN = 'LOGIN';
 
+export const SIGNUP = 'SIGNUP';
+
 const initialState = {
     login : {
         data : false
     }, 
+    signup : {
+        data : false
+    }
 };
 
-export const LoginModal = (data) => ({ type : LOGIN, data});
+export const LoginModal = (data) => ({ type : LOGIN, data });
+
+export const SignUpModal = (data) => ({ type : SIGNUP, data });
 
 
 
@@ -22,6 +29,14 @@ export default function modal(state = initialState, action) {
                     data : action.data
                 }
             }
+        case SIGNUP : {
+            return {
+                ...state,
+                signup : {
+                    data : action.data
+                }
+            }
+        }
         default : 
             return state;
     }

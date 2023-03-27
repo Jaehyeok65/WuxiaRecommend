78ffc303ceba39d9e1178bd57b7e4a0bb121d571
Lee, Jaehyeok <dlwogur15@gmail.com>
2023-03-27 20:50:38 +0900

    test : MyPage 컴포넌트 테스트 코드 작성

---
+++ src/pages/MyPage/index.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import MyPage from './MyPage';
+
+describe('MyPage component', () => {
+    it('should render button titles', () => {
+        render(<MyPage loginstate={true} />);
+        const visitButton = screen.getByText('최근 본 작품');
+        const likeButton = screen.getByText('좋아요 표시한 작품');
+        const ratingButton = screen.getByText('별점 표시한 작품');
+        expect(visitButton).toBeInTheDocument();
+        expect(likeButton).toBeInTheDocument();
+        expect(ratingButton).toBeInTheDocument();
+    });
+
+    it('should change title state on button click', () => {
+        render(<MyPage loginstate={true} />);
+        const visitButton = screen.getByText('최근 본 작품');
+        const likeButton = screen.getByText('좋아요 표시한 작품');
+        const ratingButton = screen.getByText('별점 표시한 작품');
+        fireEvent.click(likeButton);
+        expect(screen.getByText('좋아요 표시한 작품')).toHaveStyle(
+            'font-weight: bold'
+        );
+        fireEvent.click(ratingButton);
+        expect(screen.getByText('별점 표시한 작품')).toHaveStyle(
+            'font-weight: bold'
+        );
+        fireEvent.click(visitButton);
+        expect(screen.getByText('최근 본 작품')).toHaveStyle(
+            'font-weight: bold'
+        );
+    });
+});
